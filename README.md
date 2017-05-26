a1f986a07b8c50c2485e4bc6fb1af947ab24ae43
Gopinath Kesavan <gopinath.k@myntra.com>
2017-05-26 11:33:02 +0530

    updated the document

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Proxy server
 
 Reverse proxy server that caches the content in the local disk. It is usefull to mirror some content/document sites
+
+Edit the `config.js` and run `npm start`
--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 // proxy server
 exports.PROXY_ROOT = 'https://developer.mozilla.org'
 
-// regular expression to grep the proxy root (in the location header and href/src attributes)
+// regular expression to grep the proxy root (in the location header and href/src attributes of html)
 exports.PROXY_ROOT_REGX = new RegExp('https?://' + exports.PROXY_ROOT.split('//')[1], 'ig')
 
-// this local server
+// this local server. change this if any custom domain name is used
 exports.LOCAL_ROOT = 'http://localhost:8080'
 
-// directory path to save the files
+// directory path to save/serve the content
 exports.CACHE_ROOT = process.env.HOME + '/backup/html/sites'
 
 // enable or disable saving the content
